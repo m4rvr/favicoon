b3f6d6d28c23f48e7e1cb742ff543efcc8bbef09
Marvin Rudolph <rudolph.marvin@web.de>
2022-07-06 14:18:24 +0200

    refactor: add edge function

--- packages/app/astro.config.ts
+++ packages/app/astro.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config'
 import solid from '@astrojs/solid-js'
 import tailwind from '@astrojs/tailwind'
+import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solid(), tailwind({})]
+  integrations: [solid(), tailwind({})],
+  adapter: netlify()
 })
