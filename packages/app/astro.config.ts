9d9364287ced172bd6cfd153ffe183c7c5ea0e9a
Marvin Rudolph <rudolph.marvin@web.de>
2022-07-06 16:07:50 +0200

    refactor: clean up

--- packages/app/astro.config.ts
+++ packages/app/astro.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config'
 import solid from '@astrojs/solid-js'
 import tailwind from '@astrojs/tailwind'
-import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solid(), tailwind({})],
-  adapter: node(),
   vite: {
     resolve: {
       conditions: ['solid']
