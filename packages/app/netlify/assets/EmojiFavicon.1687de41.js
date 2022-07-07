54e48d9411b08d7997017de6554899155e5f5401
Marvin Rudolph <rudolph.marvin@web.de>
2022-07-07 17:39:56 +0200

    refactor: wip netlify deploy

--- packages/app/netlify/assets/EmojiFavicon.1687de41.js
+++
@@ -1,1 +0,0 @@
-import{g as t,t as e}from"./entry-client.4598e657.js";const i=e("<div>Emoji</div>");function n(){return t(i)}export{n as default};
--- packages/app/netlify/assets/TextFavicon.10af52b7.js
+++
@@ -1,1 +0,0 @@
-import{g as t,t as e}from"./entry-client.4598e657.js";const n=e("<div>Text</div>");function r(){return t(n)}export{r as default};
