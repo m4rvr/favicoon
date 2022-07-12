0f5cd0a5527786ec76487e9ee10297f2721c1114
Marvin Rudolph <rudolph.marvin@web.de>
2022-07-12 15:05:21 +0200

    refactor: wip

--- packages/app/src/trpc.ts
+++
@@ -1,6 +0,0 @@
-import { createTRPCClient } from '@trpc/client'
-import type { AppRouter } from '../../api/src/router.js'
-
-export const client = createTRPCClient<AppRouter>({
-  url: `${import.meta.env.VITE_API_BASE_URL}/trpc`
-})
