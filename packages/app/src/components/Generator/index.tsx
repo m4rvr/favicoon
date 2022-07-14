30ea8a931bb3c597b9ccd0918df150c9a2e0fde0
Marvin Rudolph <rudolph.marvin@web.de>
2022-07-14 13:03:08 +0200

    refactor: move shiki context

--- packages/app/src/components/Generator/index.tsx
+++ packages/app/src/components/Generator/index.tsx
@@ -38,48 +38,48 @@ export default function (): JSX.Element {
   // const currentType = () => types.find((type) => type.view === view())!
 
   return (
-    <HighlighterProvider>
-      <div class="relative h-full">
-        <div class="absolute z-5 top-0 left-0 right-0 flex flex-col gap-3 max-w-70 mx-auto">
-          <h3 class="text-center font-medium">Choose your type</h3>
-          <nav class="flex gap-1 items-center justify-center bg-white p-1.5 rounded-lg leading-tight shadow-lg shadow-neutral-200">
-            <For each={types}>
-              {(type) => (
-                <button
-                  class="w-1/3 px-2 py-2 rounded-md"
+    <div class="relative h-full">
+      <div class="absolute z-5 top-0 left-0 right-0 flex flex-col gap-3 max-w-70 mx-auto">
+        <h3 class="text-center font-medium">Choose your type</h3>
+        <nav class="flex gap-1 items-center justify-center bg-white p-1.5 rounded-lg leading-tight shadow-lg shadow-neutral-200">
+          <For each={types}>
+            {(type) => (
+              <button
+                class="w-1/3 px-2 py-2 rounded-md"
+                classList={{
+                  'bg-neutral-900 text-white cursor-default':
+                    type.view === view(),
+                  'cursor-default relative': isWIPType(type.view)
+                }}
+                onClick={() => {
+                  if (isWIPType(type.view)) return
+                  setView(type.view)
+                }}
+              >
+                <span
                   classList={{
-                    'bg-neutral-900 text-white cursor-default':
-                      type.view === view(),
-                    'cursor-default relative': isWIPType(type.view)
-                  }}
-                  onClick={() => {
-                    if (isWIPType(type.view)) return
-                    setView(type.view)
+                    'opacity-50': isWIPType(type.view)
                   }}
                 >
-                  <span
-                    classList={{
-                      'opacity-50': isWIPType(type.view)
-                    }}
-                  >
-                    {type.name}
+                  {type.name}
+                </span>
+                <Show when={isWIPType(type.view)}>
+                  <span class="bg-neutral-900 text-white px-[5px] pb-[1px] rounded-md absolute top-0 right-0 text-xs -translate-y-[4px] translate-x-[14px]">
+                    soon
                   </span>
-                  <Show when={isWIPType(type.view)}>
-                    <span class="bg-neutral-900 text-white px-[5px] pb-[1px] rounded-md absolute top-0 right-0 text-xs -translate-y-[4px] translate-x-[14px]">
-                      soon
-                    </span>
-                  </Show>
-                </button>
-              )}
-            </For>
-          </nav>
-        </div>
-        <div class="h-full flex items-center justify-center">
-          <Suspense fallback={<p class="text-lg font-medium">Loading...</p>}>
-            <Dynamic component={views[view()]} />
-          </Suspense>
-        </div>
+                </Show>
+              </button>
+            )}
+          </For>
+        </nav>
       </div>
-    </HighlighterProvider>
+      <div class="h-full flex items-center justify-center">
+        <Suspense fallback={<p class="text-lg font-medium">Loading...</p>}>
+          <HighlighterProvider>
+            <Dynamic component={views[view()]} />
+          </HighlighterProvider>
+        </Suspense>
+      </div>
+    </div>
   )
 }
