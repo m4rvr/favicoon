0f1fd16057cf61c0cbca25fa2a8709517c333257
Marvin Rudolph <rudolph.marvin@web.de>
2022-07-14 11:20:09 +0200

    refactor: add meta tags

--- packages/app/src/routes/404.tsx
+++ packages/app/src/routes/404.tsx
@@ -1,5 +1,12 @@
 import { type JSX } from 'solid-js'
+import { Link, Title } from 'solid-meta'
 
 export default function (): JSX.Element {
-  return <div>404</div>
+  return (
+    <>
+      <Title>Favicoon - Oh no!</Title>
+      <Link rel="canonical" href="https://favicon.netlify.app/404" />
+      <div>404</div>
+    </>
+  )
 }
--- packages/app/src/routes/index.tsx
+++ packages/app/src/routes/index.tsx
@@ -1,5 +1,12 @@
 import { type JSX } from 'solid-js'
+import { Link, Title } from 'solid-meta'
 
 export default function (): JSX.Element {
-  return <div>Index</div>
+  return (
+    <>
+      <Title>Favicoon - Favicon Tools</Title>
+      <Link rel="canonical" href="https://favicon.netlify.app/" />
+      <div>Index</div>
+    </>
+  )
 }
