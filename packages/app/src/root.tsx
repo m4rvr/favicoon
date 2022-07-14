10b261a7d58deb9498c19e872dc725c564d04053
Marvin Rudolph <rudolph.marvin@web.de>
2022-07-14 10:59:54 +0200

    refactor: use new favicon

--- packages/app/src/root.tsx
+++ packages/app/src/root.tsx
@@ -13,7 +13,6 @@ export default function Root() {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
         <Meta />
         <Links />
         <script
