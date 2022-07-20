8706cb173fe615685a212ac21e595a0c489c5764
Marvin Rudolph <rudolph.marvin@web.de>
2022-07-20 13:04:26 +0200

    chore: adjust twitter tags

--- packages/app/src/components/Generator/index.tsx
+++ packages/app/src/components/Generator/index.tsx
@@ -1,4 +1,5 @@
 import { type JSX, type ParentProps } from 'solid-js'
+import { Suspense } from 'solid-js/web'
 import Nav from './Nav.js'
 
 export default function (props: ParentProps): JSX.Element {
@@ -6,7 +7,9 @@ export default function (props: ParentProps): JSX.Element {
     <div class="relative h-full">
       <Nav />
       <div class="h-full flex items-center justify-center">
-        {props.children}
+        <Suspense fallback={<p class="font-semibold text-xl">Loading...</p>}>
+          {props.children}
+        </Suspense>
       </div>
     </div>
   )
