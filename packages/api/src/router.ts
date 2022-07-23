0fab0a23de54bee57b9c28c37cc119fe92fe2b79
Marvin Rudolph <rudolph.marvin@web.de>
2022-07-23 08:39:57 +0200

    refactor: remove unused api package

--- packages/api/src/router.ts
+++
@@ -1,9 +0,0 @@
-import * as trpc from '@trpc/server'
-
-export const appRouter = trpc.router().query('foo', {
-  async resolve() {
-    return 'It works'
-  }
-})
-
-export type AppRouter = typeof appRouter
